1bd74270c5ac93445ba9f6d6990cf07b858f5130
Gaurav Bhagat <gaurav99bhagat@gmail.com>
2022-11-02 17:09:32 +0530

    first commit

---
+++ datetime.java
@@ -0,0 +1,57 @@
+package tests;
+import java.time.*;
+
+public class datetime {
+
+	public static void main(String[] args) {
+		
+//		LocalDate d = LocalDate.now();
+//		System.out.println(d);
+//		
+//		LocalDate d2 = LocalDate.of(2020, Month.MAY, 29);
+//		System.out.println(d2);
+//		
+//		LocalDate d3 = LocalDate.parse("2022-11-28");
+//		System.out.println(d3);
+//		
+//		LocalDate d4 = d3.minusDays(50);
+//		System.out.println(d4);
+//		
+//		LocalDate d5 = d3.plusMonths(7);
+//		System.out.println(d5);
+//		
+//		DayOfWeek day = d3.getDayOfWeek();
+//		System.out.println(day);
+//		
+//		System.out.println(d3.isLeapYear());
+		
+//		LocalTime t = LocalTime.now();
+//		System.out.println(t);
+		
+//		LocalTime t2 = LocalTime.of(11, 56, 42);
+//		System.out.println(t2);
+//		
+//		System.out.println(t2.getMinute());
+		
+//		for(String a: ZoneId.getAvailableZoneIds())
+//			System.out.println(a);
+//		
+//		LocalDateTime dt = LocalDateTime.now();
+//		System.out.println(dt);
+		
+//		LocalDateTime dt2 = LocalDateTime.now(ZoneId.of("UTC"));
+//		System.out.println(dt2);
+//		
+//		String date = dt.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss"));
+//		System.out.println(date);
+		
+//		System.out.println(System.currentTimeMillis());
+		
+//		Period p = Period.between(d2, d3);
+//		System.out.println(p.getDays() + " day " + p.getMonths() + " month " + p.getYears() + " year");
+		
+//		Duration du = Duration.between(t2, t);
+//		System.out.println(du.getSeconds());		
+	}
+
+}
